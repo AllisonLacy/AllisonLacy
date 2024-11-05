0043acc8b359d4e3acc6778009df812c372dfb98
AllisonLacy <alacy2@una.edu>
2024-11-05 14:36:53 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+ {
+    gameName: "Square Game", 
+    gameURL: "https://allisonlacy.github.io/alligamerepo/", 
+    gameAuthor: "Allison Lacy",
+    gameAbout: "How many times can you click the square?",
+    gameRepo: "https://github.com/AllisonLacy/alligamerepo",
+  },
